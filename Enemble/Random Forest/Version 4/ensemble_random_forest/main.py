21f2635efd84646cdb51e85026f92657ff11e021
Kleivard <jonthan.kleivard@outlook.com>
2021-02-05 13:03:48 +0100

    New results

--- Enemble/Random Forest/Version 4/ensemble_random_forest/main.py
+++ Enemble/Random Forest/Version 4/ensemble_random_forest/main.py
@@ -7,14 +7,7 @@ Source: https://www.udemy.com/course/decision-tree-and-random-forest-python-from
 
 if __name__ == '__main__':
     dataset = "../../../../Dataset/dataset_ensemble/4.0/dataset_4.1.csv"
-    rfa0 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=10,  MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10)
-    rfa1 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=25,  MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10)
-    rfa2 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=50,  MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10)
-    rfa3 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=75,  MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10)
-    rfa4 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=100, MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10)
-    rfa5 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=125, MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10)
-    rfa6 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=150, MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10)
-    rfa7 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=250, MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10)
-    rfa8 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=500, MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10)
+    rfa4 = rf.RandomForest(DATASET=dataset, FOREST_SIZE=100, MAX_DEPTH= 100, EPOCH=3, RANDOM_SUBSPACE=10) # 30 h
+
 
 
