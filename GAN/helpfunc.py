9b42bda294d295da63811ca06de7173ae454055f
a18andtu <62876581+a18andtu@users.noreply.github.com>
2021-02-22 14:43:54 +0100

    Massa trasig GAN-kod

---
+++ GAN/helpfunc.py
@@ -0,0 +1,47 @@
+import torch
+import numpy as np
+from torch.utils.data import (TensorDataset, DataLoader, RandomSampler)
+
+def load_trainset(train_path_X, train_path_y):
+    # Load datasets from disc
+    training_data_X = torch.tensor(np.load(
+        train_path_X, allow_pickle=True))
+    training_data_y = torch.tensor(np.load(
+        train_path_y, allow_pickle=True).astype(int))
+
+    # Turn the labels from one hot to scalar: [0,1] or [1,0] to [0] or [1] (not haha)
+    # Loss function CrossEntropyLoss only takes scalar
+    # Remove line below if loss function require one hot
+
+    # Validation/train data split
+    VAL_PCT = 0.1
+    val_size = int(len(training_data_X)*VAL_PCT)
+
+    # Split train data
+    train_inputs = training_data_X[:-val_size]
+    train_labels = training_data_y[:-val_size]
+
+    val_inputs = training_data_X[-val_size:]
+    val_labels = training_data_y[-val_size:]
+
+    return train_inputs, train_labels, val_inputs, val_labels
+
+
+def data_loader(train_inputs, train_labels, val_inputs, val_labels, batch_size=64):
+    train_data = TensorDataset(train_inputs, train_labels)
+    train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+    val_data = TensorDataset(val_inputs, val_labels)
+    val_sampler = RandomSampler(val_data)
+    val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
+
+    return train_dataloader, val_dataloader
+
+def set_seed(seed_value=42):
+    """Set seed for reproducibility."""
+
+    # random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
