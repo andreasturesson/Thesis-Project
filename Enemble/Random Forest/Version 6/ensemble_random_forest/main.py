3950fb4a63f904243f7187f4beb4fe0186d0f2d6
Kleivard <jonthan.kleivard@outlook.com>
2021-03-09 10:10:15 +0100

    Changed lable on train_multiclass, added dictionary, missclassified, multiclassification
    Can display multi-classification confusion matric and classification report(F1_score). Can list what DGA family that tricked the classifier most often in a normiliezed list.

--- Enemble/Random Forest/Version 6/ensemble_random_forest/main.py
+++ Enemble/Random Forest/Version 6/ensemble_random_forest/main.py
@@ -1,52 +1,83 @@
 import pandas as pd
 import numpy as np
 import warnings
-import tqdm
-from openpyxl.workbook import Workbook
 from IPython.display import Image
 import seaborn as sns
 from matplotlib import pyplot as plt
 from itertools import product
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import precision_score, recall_score, f1_score, matthews_corrcoef, accuracy_score, confusion_matrix, plot_confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score, matthews_corrcoef, accuracy_score, \
+    confusion_matrix, plot_confusion_matrix, classification_report, ConfusionMatrixDisplay
 
 np.set_printoptions(formatter={'float:kind': '{:f}'.format})
 sns.set(rc={'figure.figsize': (8, 6)})
 
-from pandas import to_datetime
-import itertools
-import os
-import graphviz
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
-import datetime
 
 warnings.filterwarnings('ignore')
 
 PATH = 'C:\\Users\\Jonathan\\Thesis-Project\Enemble\\Random Forest\\Version 6\\ensemble_random_forest\\pic\\'
 Image(filename=PATH + "Machine Learning.png", width=900, height=900)
-
+class_names = ["Alexa_Majestic",
+                                "kraken_v2",
+                                "ramnit",
+                                "vawtrak_v2",
+                                "murofet_v3",
+                                "chinad",
+                                "ccleaner",
+                                "gozi_nasa",
+                                "zeus-newgoz",
+                                "locky",
+                                "proslikefan",
+                                "corebot",
+                                "kraken_v1",
+                                "cryptolocker",
+                                "banjori",
+                                "nymaim",
+                                "shiotob",
+                                "ramdo",
+                                "murofet_v1",
+                                "ranbyus_v1",
+                                "dircrypt",
+                                "suppobox_2",
+                                "necurs",
+                                "suppobox_3 ",
+                                "vawtrak_v1 ",
+                                "alureon ",
+                                "gozi_luther ",
+                                "matsnu ",
+                                "pykspa_noise",
+                                "pushdo",
+                                "qakbot",
+                                "fobber_v2",
+                                "vawtrak_v3",
+                                "symmi",
+                                "pykspa",
+                                "gozi_gpl",
+                                "pizd",
+                                "dyre",
+                                "bedep",
+                                "sisron",
+                                "qadars",
+                                "tempedreve",
+                                "gozi_rfc4343",
+                                "simda",
+                                "ranbyus_v2",
+                                "fobber_v1",
+                                "rovnix",
+                                "suppobox_1",
+                                "padcrypt",
+                                "murofet_v2",
+                                "tinba"]
 
 def prepareDataset():
-    df = pd.read_csv('../../../../Dataset/dataset_ensemble/5.0/dataset_5.4.csv', low_memory=False)
+    df = pd.read_csv('../../../../Dataset/dataset_ensemble/6.0/6.5/train_multiclass.csv', low_memory=False)
     labels = np.array(df['label'])
     dataset = df.drop('label', axis=1)  # Saving feature names for later use
     feature_list = list(dataset.columns)  # Convert to numpy array
     dataset = np.array(dataset)
     return train_test_split(dataset, labels, test_size=0.2, random_state=420)
-
-def plot_confusion_matrix(cm, classes=None, title='Confusion matrix'):
-    """Plots a confusion matrix."""
-    if classes is not None:
-        sns.heatmap(cm, xticklabels=classes, yticklabels=classes, vmin=0., vmax=1., annot=True, annot_kws={'size': 50})
-    else:
-        sns.heatmap(cm, vmin=0., vmax=1.)
-    plt.title(title)
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
-    plt.show()
 
 def printShape(X_train, X_test, Y_train, Y_test):
     print('X_train Shape:', X_train.shape)
@@ -71,7 +102,7 @@ def metricData(cm):
     print("The Precision per class is: ", precision)
 
     # F1 score
-    F1_score = 2*((precision * recall) / (precision + recall))
+    F1_score = 2 * ((precision * recall) / (precision + recall))
     print("The F1_score per class is: ", F1_score)
 
     # Overall accuracy
@@ -79,117 +110,116 @@ def metricData(cm):
     print("The Accuracy of each class is", ACC)
     print("")
 
-def plot_confusion_matrix(rfc):
-
-    model.fit(X_train, Y_train)
-    Y_pred = model.predict(X_test)
-
-    cm = confusion_matrix(Y_test, Y_pred)
-    cm_norm = cm / cm.sum(axis=1)[:, np.newaxis]
-    print(cm)
-    print(cm_norm)
-    plt.figure()
-    plot_confusion_matrix(cm_norm, classes=model.classes_)
-    pass
-
 def VideoGuide():
-    #model.fit(X_train, Y_train)
-    #prediction_test = model.predict(X=X_test)
+    # model.fit(X_train, Y_train)
+    # prediction_test = model.predict(X=X_test)
     # print('Classification accuracy on test set with trees = {}, max features = {} and max_depth = {}: {:.3f}'.format(n_e, m_f, m_d,accuracy_score(Y_test, prediction_test)))
-    #cm = confusion_matrix(Y_test, prediction_test)
-    #cm_norm = cm / cm.sum(axis=1)[:, np.newaxis]
+    # cm = confusion_matrix(Y_test, prediction_test)
+    # cm_norm = cm / cm.sum(axis=1)[:, np.newaxis]
 
     # plt.figure()
     # plot_confusion_matrix(cm_norm, classes=model.classes_, title='Confusion matrix accuracy on test set with trees = {}, max features = {} and max_depth = {}: {:.3f}'.format(n_e, m_f, m_d, accuracy_score(Y_test, prediction_test)))
     pass
 
-def plot():
-    # plot_precision = []
-    # plot_recall = []
-    # plot_F1_score = []
-    # plot_ACC = []
-    # x_estimators = np.array([])
-    #
-    # n_estimators = [10, 25, 50, 75, 100]
-    # max_features = 'sqrt'
-    # max_depths = [25, 50, 75, 100, None]
-    # for n_e, m_d in product(n_estimators, max_depths):
-    #     model = RandomForestClassifier(n_estimators=n_e, criterion='gini', max_features='sqrt', max_depth=m_d
-    #                                    , n_jobs=5, random_state=420)
-    #     model.fit(X_train, Y_train)
-    #     prediction_test = model.predict(X=X_test)
-    #     cm = confusion_matrix(Y_test, prediction_test)
-    #     TP = cm[1][1]
-    #     FN = cm[1][0]
-    #     FP = cm[0][1]
-    #     TN = cm[0][0]
-    #     plot_precision.append(TP / (TP + FP))
-    #     plot_recall.append(TP / (TP + FN))
-    #     plot_F1_score.append(2 * (((TP / (TP + FP)) * (TP / (TP + FN))) / ((TP / (TP + FP)) + (TP / (TP + FN)))))
-    #     plot_ACC.append((TP + TN) / (TP + FP + FN + TN))
-    #     x_estimators = np.append(x_estimators, n_e)
-    #     print(n_e)
-    #
-    # plt.plot(x_estimators, plot_precision)
-    # plt.plot(x_estimators, plot_recall)
-    # plt.plot(x_estimators, plot_F1_score)
-    # plt.plot(x_estimators, plot_ACC)
-    # plt.xticks(np.arange(min(x_estimators)-10, max(x_estimators) + 100, 100.0))
-    #
-    # plt.legend(['Prec', 'Recall', 'F1', 'ACC'], loc='upper left')
-    # plt.show()
-    pass
-
 def writeResultToExcel():
-        X_train, X_test, Y_train, Y_test = prepareDataset()
+    X_train, X_test, Y_train, Y_test = prepareDataset()
 
-        randomForest_Result = pd.DataFrame({'Trees': [],
-                                            'Depth': [],
-                                            'Features': [],
-                                            'Random_state': [],
-                                            'ACC': [],
-                                            'F1_score': [],
-                                            'Precision': [],
-                                            'Recall': []
-                                            })
-        n_estimators = []
-        max_depths = [30, 60, 90]
-        for y in range(800, 1075, 50):
-            n_estimators.append(y)
-        # for x in range(25, 500, 25):
-        #    max_depths.append(x)
-        max_depths.append(None)
-        # log2=6, sqrt= 7, 10, 13 , 47/3=16, 20,
-        max_features = [13, 15, 18, 21]
-        random_state = [1337, 4553, 412]
-        temp = 0
-        for n_e, m_d, m_f, r_s in product(n_estimators, max_depths, max_features, random_state):
-            model = RandomForestClassifier(n_estimators=n_e, criterion='gini', max_features=m_f, max_depth=m_d
-                                           , n_jobs=-1, random_state=r_s)
-            model.fit(X_train, Y_train)
-            prediction_test = model.predict(X=X_test)
-            cm = confusion_matrix(Y_test, prediction_test)
-            TP = cm[1][1]
-            FN = cm[1][0]
-            FP = cm[0][1]
-            TN = cm[0][0]
-            temp_df = pd.DataFrame({'Trees': [n_e],
-                                    'Depth': [m_d],
-                                    'Features': [m_f],
-                                    'Random_state': [r_s],
-                                    'ACC': [((TP + TN) / (TP + FP + FN + TN))],
-                                    'F1_score': [(2 * (((TP / (TP + FP)) * (TP / (TP + FN))) / (
-                                            (TP / (TP + FP)) + (TP / (TP + FN)))))],
-                                    'Precision': [(TP / (TP + FP))],
-                                    'Recall': [(TP / (TP + FN))]
-                                    })
-            randomForest_Result = randomForest_Result.append(temp_df, ignore_index=True)
+    randomForest_Result = pd.DataFrame({'Trees': [],
+                                        'Depth': [],
+                                        'Features': [],
+                                        'Random_state': [],
+                                        'ACC': [],
+                                        'F1_score': [],
+                                        'Precision': [],
+                                        'Recall': []
+                                        })
+    n_estimators = []
+    max_depths = [30, 60, 90]
+    for y in range(800, 1075, 50):
+        n_estimators.append(y)
+    # for x in range(25, 500, 25):
+    #    max_depths.append(x)
+    max_depths.append(None)
+    # log2=6, sqrt= 7, 10, 13 , 47/3=16, 20,
+    max_features = [13, 15, 18, 21]
+    random_state = [1337, 4553, 412]
+    temp = 0
+    for n_e, m_d, m_f, r_s in product(n_estimators, max_depths, max_features, random_state):
+        model = RandomForestClassifier(n_estimators=n_e, criterion='gini', max_features=m_f, max_depth=m_d
+                                       , n_jobs=-1, random_state=r_s)
+        model.fit(X_train, Y_train)
+        prediction_test = model.predict(X=X_test)
+        cm = confusion_matrix(Y_test, prediction_test)
+        TP = cm[1][1]
+        FN = cm[1][0]
+        FP = cm[0][1]
+        TN = cm[0][0]
+        temp_df = pd.DataFrame({'Trees': [n_e],
+                                'Depth': [m_d],
+                                'Features': [m_f],
+                                'Random_state': [r_s],
+                                'ACC': [((TP + TN) / (TP + FP + FN + TN))],
+                                'F1_score': [(2 * (((TP / (TP + FP)) * (TP / (TP + FN))) / (
+                                        (TP / (TP + FP)) + (TP / (TP + FN)))))],
+                                'Precision': [(TP / (TP + FP))],
+                                'Recall': [(TP / (TP + FN))]
+                                })
+        randomForest_Result = randomForest_Result.append(temp_df, ignore_index=True)
 
-            if temp != n_e:
-                print(n_e)
-            temp = n_e
+        if temp != n_e:
+            print(n_e)
+        temp = n_e
 
-        randomForest_Result.to_excel('pic/trial2.xlsx', index=False, header=True)
+    randomForest_Result.to_excel('pic/trial2.xlsx', index=False, header=True)
+
+def multi_classification():
+    X_train, X_test, Y_train, Y_test = prepareDataset()
+
+    model = RandomForestClassifier(n_estimators=10, criterion='gini', max_features=14, max_depth=125
+                                   , n_jobs=-1, random_state=1337)
+    model.fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+
+    np.set_printoptions(precision=2)
+    # Plot non-normalized confusion matrix
+    titles_options = [("Confusion matrix, without normalization", None),
+                      ("Normalized confusion matrix", 'true')]
+    for title, normalize in titles_options:
+        disp = plot_confusion_matrix(model, X_test, Y_test,
+                                     display_labels=class_names,
+                                     normalize=normalize)
+        disp.ax_.set_title(title)
+
+        # print(title)
+        # print(disp.confusion_matrix)
+
+    plt.show()
+
+    print('\nClassification Report\n')
+    print(classification_report(Y_test, Y_pred, target_names=class_names))
+
+def multi_clssification_misslcassified():
+    # Multi-classification on what families was believed to be a real domain.
+    X_train, X_test, Y_train, Y_test = prepareDataset()
+    model = RandomForestClassifier(n_estimators=10, criterion='gini', max_features=14, max_depth=125
+                                   , n_jobs=-1, random_state=1337)
+    model.fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+    missclassified = np.zeros((len(class_names), 2), dtype = object)
+    for pred, actual in zip(Y_pred, Y_test):
+        if pred == 0 and actual != 0:
+            missclassified[actual][0] = missclassified[actual][0] + 1
+    missclassified_normalized = missclassified
+    missclassified_normalized.astype(dtype=float)
+
+    sum = np.sum(missclassified)
+    for idx, (a, _) in enumerate(missclassified_normalized):
+        missclassified_normalized[idx][0] = a / sum
+        missclassified_normalized[idx][1] = class_names[idx]
+
+    # bror fråga stackoverflow nissarna
+    missclassified_normalized = missclassified_normalized[missclassified_normalized[:, 0].argsort()[::-1]]
+    print(missclassified_normalized)
 
 if __name__ == '__main__':
-    X_train, X_test, Y_train, Y_test = prepareDataset()
+    pass # the beer
