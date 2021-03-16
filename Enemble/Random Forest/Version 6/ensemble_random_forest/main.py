500e7baefe29fac88291bd0601da363396b001a8
Kleivard <jonthan.kleivard@outlook.com>
2021-03-16 23:12:10 +0100

    Problem with git.

--- Enemble/Random Forest/Version 6/ensemble_random_forest/main.py
+++ Enemble/Random Forest/Version 6/ensemble_random_forest/main.py
@@ -14,7 +14,6 @@ from sklearn.metrics import precision_score, recall_score, f1_score, matthews_co
 np.set_printoptions(formatter={'float:kind': '{:f}'.format})
 sns.set(rc={'figure.figsize': (8, 6)})
 
-
 warnings.filterwarnings('ignore')
 
 PATH = 'C:\\Users\\Jonathan\\Thesis-Project\Enemble\\Random Forest\\Version 6\\ensemble_random_forest\\pic\\'
@@ -72,7 +71,8 @@ class_names = ["Alexa_Majestic",
                                 "tinba"]
 
 def prepareDataset():
-    df = pd.read_csv('../../../../Dataset/dataset_ensemble/6.0/6.5/train_multiclass.csv', low_memory=False)
+    #df = pd.read_csv('../../../../Dataset/dataset_ensemble/6.0/6.5/train_multiclass.csv', low_memory=False)
+    df = pd.read_csv('../../../../Dataset/dataset_ensemble/train_dataset.csv', low_memory=False)
     labels = np.array(df['label'])
     dataset = df.drop('label', axis=1)  # Saving feature names for later use
     feature_list = list(dataset.columns)  # Convert to numpy array
@@ -84,42 +84,6 @@ def printShape(X_train, X_test, Y_train, Y_test):
     print('X_test Shape:', X_test.shape)
     print('Y_train Shape:', Y_train.shape)
     print('Y_test Shape:', Y_test.shape)
-
-def metricData(cm):
-    # Calculating False Positives (FP), False Negatives (FN), True Positives (TP) & True Negatives (TN)
-
-    TP = cm[1][1]
-    FN = cm[1][0]
-    FP = cm[0][1]
-    TN = cm[0][0]
-
-    # Sensitivity, hit rate, recall, or true positive rate
-    recall = TP / (TP + FN)
-    print("The Recall per class is: ", recall)
-
-    # Precision or positive predictive value
-    precision = TP / (TP + FP)
-    print("The Precision per class is: ", precision)
-
-    # F1 score
-    F1_score = 2 * ((precision * recall) / (precision + recall))
-    print("The F1_score per class is: ", F1_score)
-
-    # Overall accuracy
-    ACC = (TP + TN) / (TP + FP + FN + TN)
-    print("The Accuracy of each class is", ACC)
-    print("")
-
-def VideoGuide():
-    # model.fit(X_train, Y_train)
-    # prediction_test = model.predict(X=X_test)
-    # print('Classification accuracy on test set with trees = {}, max features = {} and max_depth = {}: {:.3f}'.format(n_e, m_f, m_d,accuracy_score(Y_test, prediction_test)))
-    # cm = confusion_matrix(Y_test, prediction_test)
-    # cm_norm = cm / cm.sum(axis=1)[:, np.newaxis]
-
-    # plt.figure()
-    # plot_confusion_matrix(cm_norm, classes=model.classes_, title='Confusion matrix accuracy on test set with trees = {}, max features = {} and max_depth = {}: {:.3f}'.format(n_e, m_f, m_d, accuracy_score(Y_test, prediction_test)))
-    pass
 
 def writeResultToExcel():
     X_train, X_test, Y_train, Y_test = prepareDataset()
@@ -134,14 +98,14 @@ def writeResultToExcel():
                                         'Recall': []
                                         })
     n_estimators = []
-    max_depths = [30, 60, 90]
-    for y in range(800, 1075, 50):
+    max_depths = [30, 60, 90, 110]
+    for y in range(800, 1001, 100):
         n_estimators.append(y)
     # for x in range(25, 500, 25):
     #    max_depths.append(x)
     max_depths.append(None)
     # log2=6, sqrt= 7, 10, 13 , 47/3=16, 20,
-    max_features = [13, 15, 18, 21]
+    max_features = ['log2', 'sqrt', 9, 13, 15, 18]
     random_state = [1337, 4553, 412]
     temp = 0
     for n_e, m_d, m_f, r_s in product(n_estimators, max_depths, max_features, random_state):
@@ -170,7 +134,7 @@ def writeResultToExcel():
             print(n_e)
         temp = n_e
 
-    randomForest_Result.to_excel('pic/trial2.xlsx', index=False, header=True)
+    randomForest_Result.to_excel('pic/trial3.xlsx', index=False, header=True)
 
 def multi_classification():
     X_train, X_test, Y_train, Y_test = prepareDataset()
@@ -221,5 +185,88 @@ def multi_clssification_misslcassified():
     missclassified_normalized = missclassified_normalized[missclassified_normalized[:, 0].argsort()[::-1]]
     print(missclassified_normalized)
 
+def writeResultToConsole():
+    X_train, X_test, Y_train, Y_test = prepareDataset()
+
+    # log2=6, sqrt= 7, 10, 13 , 47/3=16, 20,
+    max_features = ['log2', 'sqrt', 11]
+    random_state = [1337]
+    n_estimators = [500]
+    max_depths = [50, 100, 150]
+    temp = 0
+    for n_e, m_d, m_f, r_s in product(n_estimators, max_depths, max_features, random_state):
+        model = RandomForestClassifier(n_estimators=n_e, criterion='gini', max_features=m_f, max_depth=m_d
+                                       , n_jobs=-1, random_state=r_s)
+        model.fit(X_train, Y_train)
+        prediction_test = model.predict(X=X_test)
+        cm = confusion_matrix(Y_test, prediction_test)
+        TP = cm[1][1]
+        FN = cm[1][0]
+        FP = cm[0][1]
+        TN = cm[0][0]
+        print('Trees: ', n_e)
+        print('Depth: ', m_d)
+        print('Features: ', m_f)
+        print('Random_state: ', r_s)
+        print('ACC: ', ((TP + TN) / (TP + FP + FN + TN)))
+        print('F1_score: ', (2 * (((TP / (TP + FP)) * (TP / (TP + FN))) / ((TP / (TP + FP)) + (TP / (TP + FN))))))
+        print('Precision: ', (TP / (TP + FP)))
+        print('Recall: ', (TP / (TP + FN)))
+
+        if temp != n_e:
+            print(n_e)
+        temp = n_e
+
+def getMeanFromDifferentRandomSeeds():
+    # Excel sheet with different random seeds
+    excel_sheet_with_random_seed = pd.read_excel('pic/trial2.xlsx')
+    # Arrays to find all unique values for tree, depth and features
+    unique_trees = np.array([])
+    unique_depths = np.array([])
+    unique_features = np.array([])
+    # New skeleton for the excel_mean
+    excel_sheet_with_mean = pd.DataFrame({'Trees': [],
+                                          'Depth': [],
+                                          'Features': [],
+                                          'ACC_mean': [],
+                                          'F1_score_mean': [],
+                                          'Precision_mean': [],
+                                          'Recall_mean': []
+                                          })
+
+    # Find unique values for Trees, Depth, and features
+    for index, row in excel_sheet_with_random_seed.iterrows():
+        if row['Trees'] not in unique_trees:
+            unique_trees = np.append(unique_trees, row['Trees'])
+        if row['Depth'] not in unique_depths:
+            unique_depths = np.append(unique_depths, row['Depth'])
+        if row['Features'] not in unique_features:
+            unique_features = np.append(unique_features, row['Features'])
+
+    # Grid search: Looking at the unique values and if they match it will summarize ACC, F1_score, Recall and Precision
+    # Then take the summarized value and divide it with the number of rows found to get the mean value for that
+    # specific combination.
+    for u_tree, u_depth, u_feature in product(unique_trees, unique_depths, unique_features):
+        sum_ACC, sum_F1_score, sum_precision, sum_recall = np.array([]), np.array([]), np.array([]), np.array([])
+        for index, row in excel_sheet_with_random_seed.iterrows():
+            if row['Trees'] == u_tree and row['Depth'] == u_depth and row['Features'] == u_feature:
+                sum_ACC = np.append(sum_ACC, row['ACC'])
+                sum_F1_score = np.append(sum_F1_score, row['F1_score'])
+                sum_precision = np.append(sum_precision, row['Precision'])
+                sum_recall = np.append(sum_recall, row['Recall'])
+
+        temp_df = pd.DataFrame({'Trees': [u_tree],
+                                'Depth': [u_depth],
+                                'Features': [u_feature],
+                                'ACC_mean': [np.sum(sum_ACC) / sum_ACC.size],
+                                'F1_score_mean': [np.sum(sum_F1_score) / sum_F1_score.size],
+                                'Precision_mean': [np.sum(sum_precision) / sum_precision.size],
+                                'Recall_mean': [np.sum(sum_recall) / sum_recall.size]
+                                })
+
+        excel_sheet_with_mean = excel_sheet_with_mean.append(temp_df, ignore_index=True)
+
+    excel_sheet_with_mean.to_excel('pic/trial2_mean_value.xlsx', index=False, header=True)
+
 if __name__ == '__main__':
-    pass # the beer
+    writeResultToExcel()
